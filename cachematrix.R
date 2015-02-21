6d1df16a5efeec7909d3a0885699b1b4e97b6b37
Ravvshan <ravvshan@gmail.com>
2015-02-21 23:48:46 +0800

    cachematrix.R has been edited to  make two functions according to the Assignment requirements

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assognment 2
 
-## Write a short comment describing this function
+## The following function  takes an invertible matrix as an input and creates 
+## a special "matrix" object that can cache its inverse. It contains list of functions to 
+## (a) set the values of the matrix, (b) get the values of the matrix,
+## (c) calculate the inverse of the matrix, (d) get the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        iX <- NULL ## iX is the name of the inverse matrix
+        set <- function(y) {
+                x <<- y
+                iX <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) iX<<-inverse
+        getinverse <- function() iX
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The following function cacheSolve computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve the inverse 
+## from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse_matrix <- x$getinverse()
+        if(!is.null(inverse_matrix)) {
+                message("getting cached data")
+                return(inverse_matrix)
+        }
+        data <- x$get()
+        inverse_matrix <- solve(data, ...)
+        x$setinverse(inverse_matrix)
+        inverse_matrix
 }
+
+
+## To do test run (with NxN matrix with randomly generated elements) use the following commands
+## by replacing N with any (not too big) integer number you want
+## source("cachematrix.R")
+## mymatrix <- matrix(rnorm(N*N),N,N)
+## special_matrix <- makeCacheMatrix(mymatrix)
+## inverse_of_mymatrix <- cacheSolve(special_matrix)
+
